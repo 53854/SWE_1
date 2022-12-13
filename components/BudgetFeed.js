1887125ce506a95f59afa7f7093a4dbd94ad7411
Niko <nikolasm@outlook.de>
2022-12-13 12:13:53 +0100

    Budget Popup

--- components/BudgetFeed.js
+++ components/BudgetFeed.js
@@ -1,4 +1,40 @@
 import { useState } from "react";
+import Popup from 'reactjs-popup';
+
+function BudgetPopup() {
+  return(
+    <Popup
+    trigger={<button className="button"> Budget Popup </button>}
+    modal
+    nested
+    >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Budget </div>
+        <div className="content">
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+        </div>
+        <div className="actions">
+          <button
+            className="button"
+            onClick={() => {
+              close();
+            }}
+          >
+            Close Popup
+          </button>
+        </div>
+      </div>
+    )}
+  </Popup>
+  );
+}
 
 const RangeSlider = () => {
 
@@ -12,6 +48,7 @@ const RangeSlider = () => {
         <input id="budget-range" type="range" className="budget-range" min="0" max="1500" 
          onChange={(event) => setRangeval(event.target.value)} />
         <h5>{rangeval}€</h5>
+        <BudgetPopup/>
       </div>
     );
   };
